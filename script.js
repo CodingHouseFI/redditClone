f45744a6c4ca55ba82d0f25f60bd4830311ee8a1
AgileLabs <samer@agilelabs.com>
2015-06-25 13:32:47 -0700

    Routes!

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 'use strict';
 
-angular.module('reddit', [])
-.controller('MainCtrl', function($scope){
+angular.module('reddit', ["ngRoute"])
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'ListCtrl',
+      templateUrl: 'list.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+})
+.controller('ListCtrl', function($scope){
+  $scope.links = [
+    { title: 'Google', url: 'http://google.com', description: 'The google thing' },
+    { title: 'Yahoo', url: 'http://yahoo.com', description: 'The yahoo thing' }
+  ];
+
+  $scope.addLink = function() {
+    $scope.links.unshift($scope.link);
+    $scope.link = {};
+  }
 });
